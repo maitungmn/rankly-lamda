dcf9d42a52b8d007f7381f1cf621b2385d4a8d5e
maitungmn <goingdownfr@gmail.com>
2019-07-31 16:15:23 +0700

    ...

---
+++ handler.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const emojis = ["✌", "😂", "😝", "😁", "😱", "👉", "🙌", "🍻", "🔥", "🌈"];
+module.exports.rank = async event => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
+  return {
+    statusCode: 200,
+    header: {
+      "Access-Control-Allow-Origin" : "*"
+    },
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v1.0! Your function executed successfully!",
+        input: rankEmoji
+      },
+      null,
+      2
+    )
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
